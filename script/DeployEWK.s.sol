57d1579da24c64391557305f984284bac571399a
Olumuyiwa Aderemi <olumuyiwaworks02@gmail.com>
2025-10-08 17:43:55 +0100

    Update DeployEWK.s.sol

--- script/DeployEWK.s.sol
+++ script/DeployEWK.s.sol
@@ -6,9 +6,9 @@ import "../src/EscrowWithKleros.sol";
 
 contract DeployEWK is Script {
     function run() external {
-        address buyer = 0xeB07648f0f1C390bB836bE8598d43d3DfFa5258a;   // Replace with actual test addresses
+        address buyer = 0xeB07648f0f1C390bB836bE8598d43d3DfFa5258a;   
         address seller = 0xd90f32D18469Ac91DBc85FD8282047eDb406B312;
-        address arbitrator = 0x90992fb4E15ce0C59aEFfb376460Fda4Ee19C879; // KlerosLiquid address on Sepolia or mock
+        address arbitrator = 0x90992fb4E15ce0C59aEFfb376460Fda4Ee19C879; 
 
         vm.startBroadcast();
         new EscrowWithKleros{value: 0.002 ether}(buyer, seller, arbitrator);
